d3c0706b4b0c686fe896628a727e66db9821ec94
Andrew Breckenridge <asbreckenridge@me.com>
2016-06-13 18:08:26 -0700

    create shared scheme for carthage

---
+++ LocalNotificationHelper/LocalNotificationHelper.h
@@ -0,0 +1,19 @@
+//
+//  LocalNotificationHelper.h
+//  LocalNotificationHelper
+//
+//  Created by Andrew Breckenridge on 6/13/16.
+//  Copyright © 2016 Ahmet Keskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalNotificationHelper.
+FOUNDATION_EXPORT double LocalNotificationHelperVersionNumber;
+
+//! Project version string for LocalNotificationHelper.
+FOUNDATION_EXPORT const unsigned char LocalNotificationHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalNotificationHelper/PublicHeader.h>
+
+
